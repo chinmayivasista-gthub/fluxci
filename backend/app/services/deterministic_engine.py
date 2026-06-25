d087315ca51622ff89132b46ecdb4d3a207883be
chinmayivasista-gthub <chinmayivasistajob@gmail.com>
2026-06-25 21:18:20 +0530

    Implement deterministic rule engine

---
+++ backend/app/services/deterministic_engine.py
@@ -0,0 +1,35 @@
+from app.rules.python_rules import PythonRules
+from app.rules.node_rules import NodeRules
+from app.rules.docker_rules import DockerRules
+from app.rules.compiler_rules import CompilerRules
+
+from app.schemas.rule_result import RuleResult
+
+
+class DeterministicEngine:
+
+    RULES = [
+        PythonRules.module_not_found,
+        PythonRules.import_error,
+        PythonRules.pip_dependency_failure,
+
+        NodeRules.npm_dependency_failure,
+
+        DockerRules.docker_build_failure,
+
+        CompilerRules.compilation_failure,
+    ]
+
+    @classmethod
+    def analyze(cls, log: str) -> RuleResult:
+
+        for rule in cls.RULES:
+
+            result = rule(log)
+
+            if result.matched:
+                return result
+
+        return RuleResult(
+            matched=False
+        )
